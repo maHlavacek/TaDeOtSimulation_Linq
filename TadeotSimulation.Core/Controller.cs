c265f3beb5a6caa73044dfdede1c9ad040d40f94
maHlavacek <martin_hlavacek@icloud.com>
2019-02-26 10:24:30 +0100

    assumed code from TaDeOtSimulation_Simple.Controller

--- TadeotSimulation.Core/Controller.cs
+++ TadeotSimulation.Core/Controller.cs
@@ -12,6 +12,8 @@ namespace TadeotSimulation.Core
         private const int MAX_PEOPLE_PER_PRESENTATION = 20;
         private const int MAX_WAITING_MINUTES = 40;
 
+        private List<Visitor> _listOfVisitors;
+
         public event EventHandler<string> Log;
 
 
@@ -26,9 +28,17 @@ namespace TadeotSimulation.Core
         {
             string fileName = Utils.MyFile.GetFullNameInApplicationTree("Visitors.csv");
             string[] lines = File.ReadAllLines(fileName, Encoding.Default);
-
-
-
+            string[] columns;
+            for (int i = 1; i < lines.Length; i++)
+            {
+                columns = lines[i].Split(';');
+                int id = int.Parse(columns[0]);
+                int adults = int.Parse(columns[3]);
+                DateTime entryDateAndTime = DateTime.Parse(columns[1] + " " + columns[2]);
+                Visitor visitor = new Visitor(id, adults, entryDateAndTime);
+                _listOfVisitors.Add(visitor);
+            }
+            Log?.Invoke(this, $"Read {_listOfVisitors.Count} visitors with {_listOfVisitors.Sum(s => s.Adults)} adults from csv-file");
         }
 
         /// <summary>
@@ -37,8 +47,44 @@ namespace TadeotSimulation.Core
         /// </summary>
         public void StartSimulation()
         {
+            FastClock.Instance.Factor = 6000;
+            DateTime timeToStart = _listOfVisitors.Select(s => s.EntryTime).Min().AddMinutes(-60);
+            FastClock.Instance.Time = timeToStart;
+            FastClock.Instance.OneMinuteIsOver += Instance_OneMinuteIsOver;
+            Presentation.Instance.PresentationFinished += Instance_PresentationFinished;
+            FastClock.Instance.IsRunning = true;
+        }
+
+        private void Instance_OneMinuteIsOver(object sender, DateTime currentTime)
+        {
+            OnOneMinuteIsOver(currentTime);
         }
 
 
+        /// <summary>
+        /// Checks the sum of visitors and when the minimum of people per presentation is reached,
+        /// the presentation will start
+        /// </summary>
+        /// <param name="time"></param>
+        public void OnOneMinuteIsOver(DateTime time)
+        {
+            _waitingPeople = _listOfVisitors.Where(w => w.EntryTime <= time).ToList();
+            _countVisitors = _waitingPeople.Count;
+            _countPeople = _waitingPeople.Count + _waitingPeople.Sum(s => s.Adults);
+
+            if (
+                _waitingPeople.Count + _waitingPeople.Sum(s => s.Adults) >= MIN_PEOPLE_PER_PRESENTATION
+                && _presentationIsFinished
+                || _lastPresentationFinished.AddMinutes(MAX_WAITING_MINUTES) == time
+                && _waitingPeople.Count > 0
+                )
+            {
+                foreach (Visitor visitor in _waitingPeople)
+                {
+                    _listOfVisitors.Remove(visitor);
+                }
+                Presentation.Instance.StartPresentation(_waitingPeople);
+            }
+        }
     }
 }
