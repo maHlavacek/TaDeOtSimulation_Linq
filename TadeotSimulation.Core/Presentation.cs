91a9f07de20dfdd3f4df86f416cfee521ea950da
maHlavacek <martin_hlavacek@icloud.com>
2019-02-26 18:11:33 +0100

    Add some Methods and Properties

--- TadeotSimulation.Core/Presentation.cs
+++ TadeotSimulation.Core/Presentation.cs
@@ -7,11 +7,18 @@ namespace TadeotSimulation.Core
     {
         private const int PRESENTATION_MINUTES = 10;
 
+        #region Properties
+        public int CountOfPeople { get; private set; }
+        public DateTime EndOfPresentation { get; private set; }
+        public int PresentationNumber { get; private set; }
+        public DateTime StartOfPresentation { get; private set; }
+        #endregion
 
-
+        #region Methods
         public Presentation()
         {
         }
 
+        #endregion
     }
 }
---
+++ TadeotSimulation.Core/Presenter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TadeotSimulation.Core
+{
+    class Presenter
+    {
+        #region Fields
+        public event EventHandler<Presentation> PresentationStarted;
+        public event EventHandler<Presentation> PresentationFinished;
+        #endregion
+
+        #region Properties
+        public bool IsPresenting { get; set; }
+        public string Name { get; private set; }
+        public List<Presentation> Presentation { get;private set; }
+        #endregion
+
+        #region Methods
+        public Presenter(string name)
+        {
+            Name = name;
+            FastClock.Instance.OneMinuteIsOver += Instance_OneMinuteIsOver;
+        }
+
+        private void Instance_OneMinuteIsOver(object sender, DateTime currentTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartPresentation()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
